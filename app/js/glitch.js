b023d9efcd45c276b709e29da785a55fe79cd3f8
big-d-35 <44974117+big-d-35@users.noreply.github.com>
2019-12-23 19:36:08 +0300

    Desktop done

---
+++ app/js/glitch.js
@@ -0,0 +1,70 @@
+$( function() {
+		$( ".block__bckg-right" ).mgGlitch({
+          // set 'true' to stop the plugin
+				  destroy : false, 
+          // set 'false' to stop glitching
+          glitch: true, 
+          // set 'false' to stop scaling
+          scale: false, 
+          // set 'false' to stop glitch blending
+          blend : true, 
+          // select blend mode type
+          blendModeType : 'hue',
+          // set min time for glitch 1 elem
+          glitch1TimeMin : 1000, 
+          // set max time for glitch 1 elem
+          glitch1TimeMax : 800,
+          // set min time for glitch 2 elem
+          glitch2TimeMin : 100, 
+          // set max time for glitch 2 elem
+          glitch2TimeMax : 500, 
+		});
+});
+
+
+
+$( function() {
+		$( ".block__bckg-right2 " ).mgGlitch({
+          // set 'true' to stop the plugin
+				  destroy : false, 
+          // set 'false' to stop glitching
+          glitch: true, 
+          // set 'false' to stop scaling
+          scale: false, 
+          // set 'false' to stop glitch blending
+          blend : true, 
+          // select blend mode type
+          blendModeType : 'hue',
+          // set min time for glitch 1 elem
+          glitch1TimeMin : 1000, 
+          // set max time for glitch 1 elem
+          glitch1TimeMax : 800,
+          // set min time for glitch 2 elem
+          glitch2TimeMin : 100, 
+          // set max time for glitch 2 elem
+          glitch2TimeMax : 500, 
+		});
+});
+
+$( function() {
+		$( " " ).mgGlitch({
+          // set 'true' to stop the plugin
+				  destroy : false, 
+          // set 'false' to stop glitching
+          glitch: true, 
+          // set 'false' to stop scaling
+          scale: false, 
+          // set 'false' to stop glitch blending
+          blend : true, 
+          // select blend mode type
+          blendModeType : 'hue',
+          // set min time for glitch 1 elem
+          glitch1TimeMin : 1000, 
+          // set max time for glitch 1 elem
+          glitch1TimeMax : 800,
+          // set min time for glitch 2 elem
+          glitch2TimeMin : 100, 
+          // set max time for glitch 2 elem
+          glitch2TimeMax : 500, 
+		});
+});
