da8e0a5f97f08a1abb7260a884500f1e35510b7e
big-d-35 <44974117+big-d-35@users.noreply.github.com>
2019-12-27 12:25:39 +0300

    End glitch

--- app/js/glitch.js
+++ app/js/glitch.js
@@ -76,3 +76,72 @@ $( function() {
 
 
 
+
+$( function() {
+		$( ".place__map" ).mgGlitch({
+          // set 'true' to stop the plugin
+				  destroy : false, 
+          // set 'false' to stop glitching
+          glitch: true, 
+          // set 'false' to stop scaling
+          scale: true, 
+          // set 'false' to stop glitch blending
+          blend : true, 
+          // select blend mode type
+          blendModeType : 'hue',
+          // set min time for glitch 1 elem
+          glitch1TimeMin : 1000, 
+          // set max time for glitch 1 elem
+          glitch1TimeMax : 800,
+          // set min time for glitch 2 elem
+          glitch2TimeMin : 100, 
+          // set max time for glitch 2 elem
+          glitch2TimeMax : 500, 
+		});
+});
+
+$( function() {
+		$( ".names__photo" ).mgGlitch({
+          // set 'true' to stop the plugin
+				  destroy : false, 
+          // set 'false' to stop glitching
+          glitch: true, 
+          // set 'false' to stop scaling
+          scale: true, 
+          // set 'false' to stop glitch blending
+          blend : true, 
+          // select blend mode type
+          blendModeType : 'hue',
+          // set min time for glitch 1 elem
+          glitch1TimeMin : 1000, 
+          // set max time for glitch 1 elem
+          glitch1TimeMax : 800,
+          // set min time for glitch 2 elem
+          glitch2TimeMin : 100, 
+          // set max time for glitch 2 elem
+          glitch2TimeMax : 500, 
+		});
+});
+
+$( function() {
+		$( ".guest__image" ).mgGlitch({
+          // set 'true' to stop the plugin
+				  destroy : false, 
+          // set 'false' to stop glitching
+          glitch: true, 
+          // set 'false' to stop scaling
+          scale: true, 
+          // set 'false' to stop glitch blending
+          blend : true, 
+          // select blend mode type
+          blendModeType : 'hue',
+          // set min time for glitch 1 elem
+          glitch1TimeMin : 1000, 
+          // set max time for glitch 1 elem
+          glitch1TimeMax : 800,
+          // set min time for glitch 2 elem
+          glitch2TimeMin : 100, 
+          // set max time for glitch 2 elem
+          glitch2TimeMax : 500, 
+		});
+});
